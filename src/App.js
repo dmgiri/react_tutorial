cc7d743e1b9a7709170ae3887d45def54aa8cc20
Dipak M Giri <dipakgiree@gmail.com>
2021-08-17 17:58:52 +0530

    6. Unserstanding Props and PropTypes
    Added new component.
    Added  props,PropTypes and defualt proptypes values.
    Set datatypes to props.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">Text Util</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="/" tabindex="-1" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+    {/* Uncomment following lines to see errors and changes on site */}
+      {/* <NavBar title="Text Utility" about_text="About Us" /> */}
+      {/* <NavBar title="Text Utility"  about_text="About Us"/>   */}
+      <NavBar  about_text="About Us"/>
     </>
   );
 }
